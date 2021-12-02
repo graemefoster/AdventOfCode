b36a35701a7bbe963657adb99a7e4b5fc914f7df
Graeme Foster <graemefoster@gmail.com>
2021-12-02 20:39:40 +0800

    Duh - aggregate, not scan

--- src/AdventOfCode/AdventOfCodeDay2/Program.cs
+++ src/AdventOfCode/AdventOfCodeDay2/Program.cs
@@ -1,9 +1,7 @@
-using AdventOfCode;
-
 var result = File.ReadAllLines("./input.txt")
     .Select(x => x.Split(' '))
     .Select(x => (direction: x[0], amount: int.Parse(x[1])))
-    .Scan((horiz: 0, depth: 0, aim: 0), (val, next) => next.direction switch
+    .Aggregate((horiz: 0, depth: 0, aim: 0), (val, next) => next.direction switch
     {
         "forward" => (val.horiz + next.amount, val.depth + (val.aim * next.amount), val.aim),
         "down" => (val.horiz, val.depth, val.aim + next.amount),
